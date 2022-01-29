0c4c408ed243095cd92d1fc62a04ff910b9a5f58
AkmalFairuz <akmalf475@gmail.com>
2022-01-29 23:35:58 +0700

    add more information into the log file

--- src/AkmalFairuz/ErrorLog/LogAttachment.php
+++ src/AkmalFairuz/ErrorLog/LogAttachment.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace AkmalFairuz\ErrorLog;
 
+use pocketmine\utils\TextFormat;
+use function date;
+use const PHP_EOL;
+
 class LogAttachment extends \ThreadedLoggerAttachment{
 
     public function __construct(private LogWriter $writer) {
@@ -11,7 +15,7 @@ class LogAttachment extends \ThreadedLoggerAttachment{
 
     public function log($level, $message){
         if($level >= \LogLevel::WARNING) {
-            $this->writer->write($message);
+            $this->writer->write(date("Y-m-d") . " " . TextFormat::clean($message) . PHP_EOL);
         }
     }
 }
